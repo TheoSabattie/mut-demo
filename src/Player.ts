287a39122d96c4c42ab4fc1c1c9b0f32da14b7a9
Théo Sabattié <theo.sabattie@gmail.com>
2021-11-07 13:38:26 +0100

    Externalise Player behaviour in Player class

---
+++ src/Player.ts
@@ -0,0 +1,272 @@
+import { Graphics } from "@pixi/graphics";
+import { IPointData } from "@pixi/math";
+import { IUpdatable, MathTools, OVector2, UpdateService } from "math-understanding-tools";
+import { KeyboardController } from "./KeyboardController";
+
+const TELEPORT_DURATION:number = 1;
+const JUMP_FORCE:number = 250;
+const JUMP_LINEAR_UP_DURATION:number = 0.5;
+const JUMP_LINEAR_DOWN_DURATION:number = 0.5;
+const JUMP_GRAVITY_IMPULSE:number = -750;
+const SQUASH_JUMP_DOWN = {x:2, y:0.5};
+const SQUASH_JUMP_UP = {x:.75, y:1.75};
+const SQUASH_JUMP_RECEIPT = {x:1.5, y:.75};
+const SQUASH_JUMP_DOWN_SPEED:number = 10;
+const SQUASH_JUMP_UP_SPEED:number = 8;
+const SQUASH_JUMP_RECEIPT_SPEED:number = 8;
+
+const GRAVITY:number = 1500;
+const TRAIL_SIZE:number = 60;
+const TRAIL_WIDTH:number = 30;
+
+export enum PlayerMode {
+    TELEPORT = 0,
+    JUMP_LINEAR = 1,
+    JUMP_GRAVITY = 2,
+    JUMP_GRAVITY_SQUASH = 3,
+    JUMP_GRAVITY_SQUASH_FX = 4
+}
+
+function inverseLerp(pValue:number, pA:number, pB:number):number{
+    return (pValue - pA) / (pB - pA);
+}
+
+function remap(pValue:number, pFromA:number, pFromB:number, pToA:number, pToB:number){
+    return MathTools.lerp(pToA, pToB, inverseLerp(pValue, pFromA, pFromB));
+}
+
+export class Player extends Graphics implements IUpdatable {
+    private _keyboardController:KeyboardController;
+    private _doAction:Function;
+    private _mode:PlayerMode = PlayerMode.JUMP_GRAVITY_SQUASH_FX;
+    private _toY:number = 0;
+    private _fromY:number = 0;
+    private _yVelocity:number = 0;
+    private _particles:Array<IPointData> = [];
+    private _trail:Graphics = new Graphics();
+
+    public constructor(pKeyboardController:KeyboardController){
+        super();
+        this.beginFill(0xFF0000);
+        this.drawRect(-50, -100, 100, 100);
+        this.endFill();
+        this._keyboardController = pKeyboardController;
+        this._setModeNormal();
+        this.on("added", this._onAdded);
+    }
+
+    public get mode():PlayerMode {
+        return this._mode;
+    }
+
+    public set mode(pMode:PlayerMode) {
+        if (pMode != PlayerMode.JUMP_GRAVITY_SQUASH_FX)
+            this._trail.clear();
+        else {
+            this._resetParticlesPositions();
+        }
+
+        this._mode = pMode;
+    }
+
+    public start():void {
+        UpdateService.add(this);
+    }
+
+    private _onAdded(pEvent:Event):void {
+        this.parent.addChildAt(this._trail, this.parent.getChildIndex(this));
+        this._resetParticlesPositions();
+    }
+
+    private _resetParticlesPositions():void {
+        if (this._particles.length > 0){
+            this._particles = [];
+        }
+
+        for (var i:number = 0; i < TRAIL_SIZE; i++)
+            this._particles.push(this.position.clone());
+    }
+
+    private _doActionNormal():void {
+        if (this._mode == PlayerMode.JUMP_GRAVITY_SQUASH || this._mode == PlayerMode.JUMP_GRAVITY_SQUASH_FX){
+            let lScale = OVector2.moveTowards(this.scale, new OVector2(1,1), UpdateService.deltaTime*SQUASH_JUMP_RECEIPT_SPEED);
+            this.scale.set(lScale.x, lScale.y);
+        }
+
+        if (this._keyboardController.isJustDown("Space")){
+            this._setModeJumpDown();
+        }
+    }
+
+    private _setModeJumpDown():void  {
+        this._resetElapsedTime();
+        this._doAction = this._doActionJumpDown;
+    }
+
+    private _setModeJump():void {
+        this._yVelocity = JUMP_GRAVITY_IMPULSE;
+        this._fromY = this.y;
+        this._toY = this.y - JUMP_FORCE;
+        this._resetElapsedTime();
+        this._doAction = this._doActionJump;
+    }
+
+    private _setModeReceipt():void {
+        this._resetElapsedTime();
+        this._doAction = this._doActionReceipt;
+    }
+
+    private _setModeNormal():void {
+        this._doAction = this._doActionNormal;
+    }
+
+    private _setModeFall():void {
+        this._resetElapsedTime();
+        this._doAction = this._doActionFall;
+    }
+
+    private _elapsedTime:number;
+
+    private _resetElapsedTime():void{
+        this._elapsedTime = 0;
+    }
+
+    private _updateElapsedTime():void {
+        this._elapsedTime += UpdateService.deltaTime;
+    }
+
+    private _reachTime(pTime:number):boolean {
+        return this._elapsedTime >= pTime;
+    }
+
+    private _ratioTime(pTime:number):number {
+        return this._elapsedTime / pTime;
+    }
+
+    private _doActionJumpDown():void {
+        if (this._mode == PlayerMode.JUMP_GRAVITY_SQUASH || this._mode == PlayerMode.JUMP_GRAVITY_SQUASH_FX){
+            let lScale = OVector2.moveTowards(this.scale, SQUASH_JUMP_DOWN, UpdateService.deltaTime*SQUASH_JUMP_DOWN_SPEED);
+            this.scale.set(lScale.x, lScale.y);
+        }
+
+        if (this._keyboardController.isJustUp("Space")){
+            this._setModeJump();
+        }
+    }
+
+    private _doActionJump():void {
+        if (this._mode == PlayerMode.TELEPORT || this._mode == PlayerMode.JUMP_LINEAR){
+            let lDuration:number = 0;
+            
+            if (this._mode == PlayerMode.TELEPORT){
+                this.y = this._toY;
+                lDuration = TELEPORT_DURATION;
+            } else if (this._mode == PlayerMode.JUMP_LINEAR){
+                lDuration = JUMP_LINEAR_UP_DURATION;
+                this.y = MathTools.lerp(this._fromY, this._toY, this._ratioTime(JUMP_LINEAR_UP_DURATION));
+            }
+
+            if (this._reachTime(lDuration)){
+                this._setModeFall();
+                return;
+            }
+        } else {
+            if (this._mode == PlayerMode.JUMP_GRAVITY_SQUASH || this._mode == PlayerMode.JUMP_GRAVITY_SQUASH_FX){
+                let lTargetedScale:OVector2 = OVector2.lerp(SQUASH_JUMP_UP, new OVector2(1,1), inverseLerp(this._yVelocity, JUMP_GRAVITY_IMPULSE, 0));
+                let lScale = OVector2.moveTowards(this.scale, lTargetedScale, UpdateService.deltaTime*SQUASH_JUMP_UP_SPEED);
+                this.scale.set(lScale.x, lScale.y);
+            }
+            
+            this.y += this._yVelocity * UpdateService.deltaTime;
+            this._yVelocity += GRAVITY * UpdateService.deltaTime;
+
+            if (this._yVelocity >= 0){
+                this._setModeFall();
+                return;
+            }
+        }
+
+        this._updateElapsedTime();
+    }
+
+    private _doActionFall():void {
+        if (this._mode == PlayerMode.TELEPORT || this._mode == PlayerMode.JUMP_LINEAR){
+            let lDuration:number = 0;
+
+            if (this._mode == PlayerMode.TELEPORT){
+                this.y = this._fromY;
+            } else if (this._mode == PlayerMode.JUMP_LINEAR){
+                lDuration = JUMP_LINEAR_DOWN_DURATION;
+                this.y = MathTools.lerp(this._toY, this._fromY, this._ratioTime(lDuration));
+            }
+
+            if (this._reachTime(lDuration)){
+                this._setModeReceipt();
+                return;
+            }
+        } else {
+            if (this._mode == PlayerMode.JUMP_GRAVITY_SQUASH || this._mode == PlayerMode.JUMP_GRAVITY_SQUASH_FX){
+                let lTargetedScale:OVector2 = OVector2.lerp(SQUASH_JUMP_UP, new OVector2(1,1), inverseLerp(Math.abs(this._yVelocity), -JUMP_GRAVITY_IMPULSE, 50));
+                let lScale = OVector2.moveTowards(this.scale, lTargetedScale, UpdateService.deltaTime*SQUASH_JUMP_UP_SPEED);
+                this.scale.set(lScale.x, lScale.y);
+            }
+
+            this.y += this._yVelocity * UpdateService.deltaTime;
+            this._yVelocity += GRAVITY * UpdateService.deltaTime;
+
+            if (this.y >= this._fromY){
+                this.y = this._fromY;
+                this._setModeReceipt();
+                return;
+            }
+        }
+
+        this._updateElapsedTime();
+    }
+
+    private _doActionReceipt():void {
+        if (this._mode == PlayerMode.JUMP_GRAVITY_SQUASH || this._mode == PlayerMode.JUMP_GRAVITY_SQUASH_FX){
+            let lScale = OVector2.moveTowards(this.scale, SQUASH_JUMP_RECEIPT, UpdateService.deltaTime*SQUASH_JUMP_RECEIPT_SPEED);
+            this.scale.set(lScale.x, lScale.y);
+
+            if (this.scale.x == SQUASH_JUMP_RECEIPT.x && this.scale.y == SQUASH_JUMP_RECEIPT.y)
+                this._setModeNormal();
+        } else {
+            let lDuration:number = 0;
+
+            if (this._reachTime(lDuration)){
+                this._setModeNormal();
+                return;
+            }
+    
+            this._updateElapsedTime();
+        }
+    }
+
+    public update():void {
+        this._doAction();
+
+        let lParticles = this._particles;
+        lParticles.shift();
+        lParticles.push(this.position.clone());
+    
+        if (this._mode == PlayerMode.JUMP_GRAVITY_SQUASH_FX){
+            let lTrail = this._trail;
+            lTrail.clear();
+            lTrail.beginFill(0xFFFFFF);
+            lTrail.moveTo(lParticles[0].x + TRAIL_WIDTH, lParticles[0].y);
+
+            for (var i:number = 1; i < TRAIL_SIZE; i++){
+                lTrail.beginFill(0xBBBBBB, i/TRAIL_SIZE);
+                lTrail.lineTo(lParticles[i].x + TRAIL_WIDTH * i/TRAIL_SIZE, lParticles[i].y);
+                lTrail.lineTo(lParticles[i].x - TRAIL_WIDTH * i/TRAIL_SIZE, lParticles[i].y);
+                lTrail.lineTo(lParticles[i-1].x - TRAIL_WIDTH * (i-1)/TRAIL_SIZE, lParticles[i-1].y);
+                lTrail.lineTo(lParticles[i-1].x + TRAIL_WIDTH * (i-1)/TRAIL_SIZE, lParticles[i-1].y);
+                lTrail.moveTo(lParticles[i].x + TRAIL_WIDTH * i/TRAIL_SIZE, lParticles[i].y);
+            }
+        
+            lTrail.closePath();
+            lTrail.endFill();
+        }
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,26 @@
 import { Application } from  "pixi.js";
 import { EventTypes, Graphic, GraphicGrid, GraphicRectangle, GraphicVector, MathMapWindow, ORectTransform, OVector2, UpdateService } from  "math-understanding-tools";
 import { Graphics } from "pixi.js";
+import { sound, filters } from "@pixi/sound";
 import { KeyboardController } from "./KeyboardController";
 import { gsap } from "gsap";
 import { Button, FilledAreaButton } from "./Button";
 import { Text } from "./Text";
 import { RootArea, Area, FilledArea } from "./Area";
+import { Player, PlayerMode } from "./Player";
 
 let options = {
     antialias:true,
     autoResize:true
 };
+
+const SOUNDS = [
+    sound.add("pop0", "pop.ogg"),
+    sound.add("pop1", "pop2.wav"),
+    sound.add("pop2", "pop3.wav")
+];
+
+let soundSet = SOUNDS.slice();
 
 const app    = new Application(options);
 var keyboardController:KeyboardController = new KeyboardController(window);
@@ -18,6 +28,23 @@ var mathMapWindow:MathMapWindow = new MathMapWindow(app.stage, 50);
 mathMapWindow.rectTransform.pivot.setTo(.5,.5);
 mathMapWindow.rectTransform.anchorMin.setTo(0,0);
 mathMapWindow.rectTransform.anchorMax.setTo(1,1);
+
+UpdateService.add(gameloop);
+
+function gameloop():void {
+    if (keyboardController.isJustDown("ArrowRight")){
+        player.mode = (player.mode + 1) %5;
+    } else if (keyboardController.isJustDown("ArrowLeft")){
+        player.mode = ((player.mode - 1)+5)%5;
+    }
+}
+
+let player = new Player(keyboardController);
+player.x = -150;
+player.y = -50;
+
+player.mode = PlayerMode.TELEPORT;
+
 var position:OVector2 = new OVector2(-3,-1);
 
 var trail:Graphics = new Graphics();
@@ -30,8 +57,7 @@ platform.fillStyle.color = 0x888888;
 platform.fillStyle.alpha = 1;
 mathMapWindow.map.grid.lineStyle.alpha = 0;
 
-var rect = new Graphics();
-mathMapWindow.map.objectsContainer.addChild(rect);
+mathMapWindow.map.objectsContainer.addChild(player);
 /*var button:Button = new Button(mathMapWindow.container);
 
 button.lineStyle.setAlpha(0);
@@ -43,7 +69,7 @@ button.rectTransform.anchoredPosition.setTo(-25, 25);
 button.text.text = "";*/
 
 let root:RootArea = new RootArea(app.stage);
-
+/*
 let uiEl = new FilledArea(root);
 uiEl.rectTransform.anchorMax.setTo(.5,1);
 uiEl.fillStyle.color = 0xFF0FF00
@@ -51,25 +77,14 @@ uiEl.fillStyle.color = 0xFF0FF00
 let button = new FilledAreaButton(uiEl);
 button.interactable = false;
 
-/*
-let wind = new Window(root);
-wind.rectTransform.anchorMax.setTo(.5,1);
-*/
 let text:Text = new Text(uiEl);
 console.log(root.rectTransform.rect);
 text.rectTransform.anchorMin.setTo(.5,.5);
 text.rectTransform.anchorMax.setTo(.5,.5);
 text.rectTransform.pivot.setTo(.5,.5);
+*/
 
-rect.position.set(position.x*50, position.y*50);
-rect.beginFill(0xFF0000);
-rect.drawRect(-50, -100, 100, 100);
-position.addListener(EventTypes.CHANGE, ()=>{
-    rect.x = position.x * 50;
-    rect.y = position.y * 50;
-});
-
-mathMapWindow.map.addVector(new OVector2(), position);
+var positionVector:GraphicVector = mathMapWindow.map.addVector(new OVector2(), position.clone());
 
 document.addEventListener("DOMContentLoaded", init);
 window.addEventListener("resize", resize);
@@ -79,81 +94,53 @@ function init(_:Event):void {
         UpdateService.update(app.ticker.deltaMS/1000);
     });
     document.body.appendChild(app.view);
+    appear(getClickPromise());
     resize();
 }
 
-UpdateService.add(gameloop);
-var particles:Array<OVector2> = [];
+function getClickPromise():Promise<void> {
+    return new Promise<void>(result => {
+        function click(pEvent:MouseEvent):void {
+            window.removeEventListener("click", click);
+            result();
+        }
 
-const CELL_SIZE:number = 50;
-const TRAIL_SIZE:number = 60;
-
-for (var i:number = 0; i < TRAIL_SIZE; i++){
-    particles.push(position.clone());
+        window.addEventListener("click", click);
+    });
 }
 
-trail.beginFill(0xFFFFFF);
-trail.lineStyle({
-    color:0xFFFFFF,
-    alpha:1,
-    width:4
-})
+function playPopSound(){
+    if (soundSet.length == 0)
+        soundSet = SOUNDS.slice();
 
-function gameloop():void {
-    if (keyboardController.isDown("Space")){
-        jump();
-    }
-
-    particles.shift();
-    particles.push(position.clone());
-
-    trail.clear();
-    trail.moveTo(particles[0].x * CELL_SIZE, particles[0].y * CELL_SIZE);
-
-    for (var i:number = 1; i < TRAIL_SIZE; i++){
-        trail.beginFill(0xBBBBBB, i/TRAIL_SIZE);
-        trail.lineTo(particles[i].x * CELL_SIZE - i/TRAIL_SIZE * 30, particles[i].y * CELL_SIZE);
-        trail.lineTo(particles[i].x * CELL_SIZE + i/TRAIL_SIZE * 30, particles[i].y * CELL_SIZE);
-        trail.lineTo(particles[i-1].x * CELL_SIZE + (i-1)/TRAIL_SIZE * 30, particles[i-1].y * CELL_SIZE);
-        trail.lineTo(particles[i-1].x * CELL_SIZE - (i-1)/TRAIL_SIZE * 30, particles[i-1].y * CELL_SIZE);
-        trail.moveTo(particles[i].x * CELL_SIZE - i/TRAIL_SIZE * 30, particles[i].y * CELL_SIZE);
-    }
-
-    trail.closePath();
-    trail.endFill();
+    let lRandomIndex:number = Math.floor(Math.random() * soundSet.length);
+    let lRandomSound = soundSet[lRandomIndex];
+    soundSet.splice(lRandomIndex, 1);
+    lRandomSound.play();
 }
 
-let isJumping = false;
-
-function jump(){
-    if (isJumping)
-        return;
-
-    isJumping = true;
-    // TODO : juming movement
-
+async function appear(pPromise:Promise<void>){
     let timeline:gsap.core.Timeline = gsap.timeline();
-    // teleport
-    // timeline.set(position, {y:-5});
-    // timeline.set(position, {y:-1, delay:1, onComplete:()=>{isJumping = false}});
+    timeline.call(playPopSound);
+    timeline.from(mathMapWindow.map.originCircle, {radius:0, ease:"back.out(5)", onComplete:playPopSound});
+    timeline.from([mathMapWindow.map.xAxis, mathMapWindow.map.yAxis], {magnitude:0.000001, ease:"back.out(2)", onComplete:playPopSound});
+    timeline.from(platform, {height:0, width:0, ease:"back.out(2)", onComplete:playPopSound});
+    timeline.from(player, {height:0, width:0, ease:"back.out(2)", onComplete:playPopSound});
+    timeline.from(positionVector, {magnitude:0.000001, onComplete:function(){
+        positionVector.to = position;
+        player.start();
+        UpdateService.add(keyboardController);
 
-    // linear
-    // timeline.to(position, {y:-5, duration:0.5, ease:"none"});
-    // timeline.to(position, {y:-1, duration:0.5, ease:"none", onComplete:()=>{isJumping = false}});
-    
-    // sympa
-    // timeline.to(position, {y:-5, duration:.5, ease:"sine.out"});
-    // timeline.to(position, {y:-1, duration:.5, ease:"sine.in", onComplete:()=>{isJumping = false}});
-
-    // décollage
-    timeline.to(rect.scale, {x:2, y:0.5, duration:.15});
-    timeline.to(rect.scale, {x:.75, y:1.75, duration:.15});
-    timeline.to(position, {y:-5, duration:.5, ease:"sine.out", delay:"-0.15"});
-    timeline.to(rect.scale, {y:1, x:1, duration:.3, delay:"-0.3"});
-    timeline.to(position, {y:-1, duration:.5, ease:"sine.in", delay:"-0.1"});
-    timeline.to(rect.scale, {x:.75, y:1.75, duration:.3, delay:"-0.3"});
-    timeline.to(rect.scale, {x:1.5, y:.75, duration:.1, ease:"sin.out"});
-    timeline.to(rect.scale, {x:1, y:1, duration:.1, ease:"sin.in", onComplete:()=>{isJumping = false;}});
+        let lCB = player.position.cb.bind(player.transform); 
+        player.position.cb = function(){
+            position.setTo(player.position.x / 50, player.position.y / 50);
+            (<any>positionVector)._draw();
+            lCB();
+        };
+    }});
+    timeline.pause();
+    await pPromise;
+    timeline.play();
 }
 
 function resize(_?:Event):void {
