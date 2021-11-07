00f263f98c0b5f3242d27caf8518bfa660ba55e4
Théo Sabattié <theo.sabattie@gmail.com>
2021-11-07 13:38:08 +0100

    KeyboardController: add way to detect when the key is right now down (in current frame)

--- src/KeyboardController.ts
+++ src/KeyboardController.ts
@@ -1,7 +1,10 @@
+import { IUpdatable, UpdateService } from "math-understanding-tools";
 import { KeyboardEventName } from "./KeyboardEventName";
 
-export class KeyboardController implements EventListenerObject {
+export class KeyboardController implements EventListenerObject, IUpdatable {
     private _keysToIsDown:{[key:string]:boolean} = {};
+    private _keysToJustDown:{[key:string]:boolean} = {};
+    private _keysToJustUp:{[key:string]:boolean} = {};
     private _window:Window;
 
     public constructor(pWindow:Window){
@@ -10,12 +13,26 @@ export class KeyboardController implements EventListenerObject {
         pWindow.addEventListener(KeyboardEventName.KEY_UP, this);
     }
 
+    public update(): void {
+        for (let key in this._keysToJustDown)
+            this._keysToJustDown[key] = false;
+
+        for (let key in this._keysToJustUp)
+            this._keysToJustUp[key] = false;
+    }
+
     public handleEvent(pEvent:Event):void {
         let lKeyboardEvent:KeyboardEvent = <KeyboardEvent> pEvent;
         
         if (pEvent.type == KeyboardEventName.KEY_DOWN){
+            if(!this._keysToIsDown[lKeyboardEvent.code])
+                this._keysToJustDown[lKeyboardEvent.code] = true;
+
             this._keysToIsDown[lKeyboardEvent.code] = true;
         } else if (pEvent.type == KeyboardEventName.KEY_UP){
+            if(this._keysToIsDown[lKeyboardEvent.code])
+                this._keysToJustUp[lKeyboardEvent.code] = true;
+
             this._keysToIsDown[lKeyboardEvent.code] = false;
         }
     }
@@ -24,8 +41,16 @@ export class KeyboardController implements EventListenerObject {
         return this._keysToIsDown[pCode];
     }
 
-    public isUp(pCode:number):boolean {
-        return this._keysToIsDown[pCode];
+    public isUp(pCode:string):boolean {
+        return !this._keysToIsDown[pCode];
+    }
+
+    public isJustDown(pCode:string):boolean {
+        return this._keysToJustDown[pCode];
+    }
+
+    public isJustUp(pCode:string):boolean {
+        return this._keysToJustUp[pCode];
     }
 
     public destroy():void {
